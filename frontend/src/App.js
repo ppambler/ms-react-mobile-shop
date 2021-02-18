4d7d240ed87ab262a232f95f425db02284c02b4f
ppambler <allinlbj@gmail.com>
2021-02-18 11:15:51 +0800

    页面基本骨架

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      <Footer />
     </>
   );
 }
